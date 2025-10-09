ab5c9ffc60be32836b658acec0015dc777877c17
Jarot-Fierro <140451417+Jarot-Fierro@users.noreply.github.com>
2025-10-09 07:41:28 -0300

    Refactor movimiento_ficha to pacientes, update serializers and viewsets for improved filtering and user-specific data access

--- kardex/config/urls.py
+++ kardex/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from kardex.views.api.movimiento_ficha import router
+from kardex.views.api.pacientes import router
 from kardex.views.home import HomeDashboardView
 
 urlpatterns = [
---
+++ kardex/kardex/views/api/pacientes.py
@@ -0,0 +1,43 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import routers
+from rest_framework import serializers
+from rest_framework import viewsets, filters
+
+from kardex.models import Paciente, IngresoPaciente
+
+
+class PacienteSerializer(serializers.ModelSerializer):
+    establecimiento = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Paciente
+        fields = '__all__'
+
+    def get_establecimiento(self, obj):
+        # Devuelve solo el establecimiento del ingreso actual (del usuario)
+        user = self.context['request'].user
+        ingreso = IngresoPaciente.objects.filter(
+            paciente=obj, establecimiento=user.establecimiento
+        ).first()
+        if ingreso:
+            return ingreso.establecimiento.nombre
+        return None
+
+
+class PacienteViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = PacienteSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['rut', 'codigo', 'nombre']
+    queryset = Paciente.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        ingresos = IngresoPaciente.objects.filter(
+            establecimiento=user.establecimiento
+        ).values_list('paciente_id', flat=True)
+
+        return Paciente.objects.filter(id__in=ingresos).order_by('id')
+
+
+router = routers.DefaultRouter()
+router.register(r'ingreso-paciente', PacienteViewSet)
